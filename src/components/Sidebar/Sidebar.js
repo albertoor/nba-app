ae32d0acc2d9f7bb521a4985e9b00ab3dee254ae
albertoornelesdev <albertoornelasdev@gmail.com>
2021-08-28 22:55:25 -0700

    Adding showSidebar funciont

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,61 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import logo from "./ballon.png";
 import { Link } from "react-router-dom";
+import { FaTimes, FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const handleClick = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="sidebar-welcome">
-        <h3>Welcome to BaMaster Basketball App</h3>
-        <img src={logo} alt="logo" srcset="" />
-        <figcaption>
-          <p>Icon by </p>
-          <a href="https://www.flaticon.com/authors/prettycons">PrettyIcons</a>
-        </figcaption>
-      </div>
+    <>
+      <IconContext.Provider value={{ color: "black" }}>
+        <div className="sidebar">
+          <div className="menu-icon" onClick={handleClick}>
+            {showSidebar ? <FaBars /> : <FaTimes />}
+          </div>
+          {showSidebar ? (
+            <h1>Hello</h1>
+          ) : (
+            <div className="sidebar-menu">
+              <div className="sidebar-welcome">
+                <h3>Welcome to BaMaster Basketball App</h3>
+                <img src={logo} alt="logo" srcset="" />
+                <figcaption>
+                  <p>Icon by </p>
+                  <a href="https://www.flaticon.com/authors/prettycons">
+                    PrettyIcons
+                  </a>
+                </figcaption>
+              </div>
 
-      <div className="sidebar-options">
-        <div className="sidebar-option">
-          <div className="sidebar-option-header main-option-header">
-            <h4>Players</h4>
-          </div>
-          <div className="sidebar-option-choose-option">
-            <Link to="/player">➡️ Get Specific Player</Link>
-            <Link to="/players">➡️ Get All Players</Link>
-          </div>
-        </div>
+              <div className="sidebar-options">
+                <div className="sidebar-option">
+                  <div className="sidebar-option-header main-option-header">
+                    <h4>Players</h4>
+                  </div>
+                  <div className="sidebar-option-choose-option">
+                    <Link to="/player">➡️ Get Specific Player</Link>
+                    <Link to="/players">➡️ Get All Players</Link>
+                  </div>
+                </div>
 
-        <div className="sidebar-option">
-          <div className="sidebar-option-header">
-            <h4>Teams</h4>
-          </div>
-          <div className="sidebar-option-choose-option">
-            <Link to="/teams">➡️ Get All Teams</Link>
-            <Link to="/team">➡️ Get a Specific Team</Link>
-          </div>
-        </div>
+                <div className="sidebar-option">
+                  <div className="sidebar-option-header">
+                    <h4>Teams</h4>
+                  </div>
+                  <div className="sidebar-option-choose-option">
+                    <Link to="/teams">➡️ Get All Teams</Link>
+                    <Link to="/team">➡️ Get a Specific Team</Link>
+                  </div>
+                </div>
 
-        <div className="sidebar-option">
-          <div className="sidebar-option-header">
-            <h4>Games</h4>
-          </div>
-          <div className="sidebar-option-choose-option">
-            <Link to="/game">➡️ Get a Specific Game</Link>
-            <Link to="/games">➡️ Get All Games</Link>
-          </div>
-        </div>
+                <div className="sidebar-option">
+                  <div className="sidebar-option-header">
+                    <h4>Games</h4>
+                  </div>
+                  <div className="sidebar-option-choose-option">
+                    <Link to="/game">➡️ Get a Specific Game</Link>
+                    <Link to="/games">➡️ Get All Games</Link>
+                  </div>
+                </div>
 
-        <div className="sidebar-option">
-          <div className="sidebar-option-header">
-            <h4>Stats</h4>
-          </div>
-          <div className="sidebar-option-choose-option">
-            <Link to="/stats">➡️ Get All Stats</Link>
-          </div>
+                <div className="sidebar-option">
+                  <div className="sidebar-option-header">
+                    <h4>Stats</h4>
+                  </div>
+                  <div className="sidebar-option-choose-option">
+                    <Link to="/stats">➡️ Get All Stats</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-      </div>
-    </div>
+      </IconContext.Provider>
+    </>
   );
 };
 
