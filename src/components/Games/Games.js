5946df239099dc44b0b53594575ee0c2b9e005c1
albertoornelesdev <albertoornelasdev@gmail.com>
2021-09-03 22:03:49 -0700

    structuring games data

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,5 +1,7 @@
 import React from "react";
 import useGames from "../../hooks/useGames";
+import "./Games.css";
+import renderIcon from "../renderIcon";
 
 const Games = () => {
   const games = useGames();
@@ -8,9 +10,24 @@ const Games = () => {
     <div>
       {games.map((game) => (
         <div className="game-container">
-          <h1>
-            {game.home_team.abbreviation} vs {game.visitor_team.abbreviation}
-          </h1>
+          <div className="home-team">
+            <i>{renderIcon(game.home_team.abbreviation)}</i>
+            <h1>{game.home_team.full_name}</h1>
+            <h3>{game.home_team_score}</h3>
+          </div>
+          <div className="versus">
+            <h1>VS</h1>
+          </div>
+          <div className="visitor-team">
+            <i>{renderIcon(game.visitor_team.abbreviation)}</i>
+            <h1>{game.visitor_team.full_name}</h1>
+            <h3>{game.visitor_team_score}</h3>
+          </div>
+          <div className="more-info">
+            <p>{game.season}</p>
+            <p>{game.status}</p>
+            <p>{game.date}</p>
+          </div>
         </div>
       ))}
     </div>
