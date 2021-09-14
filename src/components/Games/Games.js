ac0d32316c861bff8740a3445cd0dd6554e4e8be
albertoornelesdev <albertoornelasdev@gmail.com>
2021-09-13 19:49:06 -0700

    Games styles

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -2,31 +2,37 @@ import React from "react";
 import useGames from "../../hooks/useGames";
 import "./Games.css";
 import renderIcon from "../renderIcon";
+import Moment from "react-moment";
 
 const Games = () => {
   const games = useGames();
 
   return (
-    <div>
+    <div className="games">
       {games.map((game) => (
         <div className="game-container">
-          <div className="home-team">
-            <i>{renderIcon(game.home_team.abbreviation)}</i>
-            <h1>{game.home_team.full_name}</h1>
-            <h3>{game.home_team_score}</h3>
+          <div className="date">
+            <Moment date={game.date} />
           </div>
-          <div className="versus">
-            <h1>VS</h1>
+
+          <div className="versus-section">
+            <div className="home-team">
+              <i>{renderIcon(game.home_team.abbreviation)}</i>
+              <h3>{game.home_team.full_name}</h3>
+              <h1>{game.home_team_score}</h1>
+            </div>
+            <div className="versus">
+              <h1>-</h1>
+            </div>
+            <div className="visitor-team">
+              <h1>{game.visitor_team_score}</h1>
+              <h3>{game.visitor_team.full_name}</h3>
+              <i>{renderIcon(game.visitor_team.abbreviation)}</i>
+            </div>
           </div>
-          <div className="visitor-team">
-            <i>{renderIcon(game.visitor_team.abbreviation)}</i>
-            <h1>{game.visitor_team.full_name}</h1>
-            <h3>{game.visitor_team_score}</h3>
-          </div>
+
           <div className="more-info">
-            <p>{game.season}</p>
             <p>{game.status}</p>
-            <p>{game.date}</p>
           </div>
         </div>
       ))}
